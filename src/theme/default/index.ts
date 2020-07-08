344c7cf1392622ea008e89c5edaeb1b474e7872c
xiaohulu <idocument@qq.com>
2020-07-08 17:35:35 +0800

    feat: 新增 theme 文件夹

---
+++ src/theme/default/index.ts
@@ -0,0 +1,11 @@
+import * as app from "./app.m.css";
+import * as defaultVariant from "./variants/default.m.css";
+
+export default {
+	theme: {
+		"mini-program-component/app": app,
+	},
+	variants: {
+		default: defaultVariant,
+	},
+};
